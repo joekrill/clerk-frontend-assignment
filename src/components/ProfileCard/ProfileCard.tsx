d3530bcdf247a16b851ed39e44ce92f533b94d36
Joe Krill <joe@joekrill.com>
2024-03-27 12:38:44 -0400

    Ensure card text is readable

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,3 +1,4 @@
+import tinyColor from "tinycolor2";
 export interface ProfileCardProps {
   backgroundColor?: string;
   email: string;
@@ -7,6 +8,34 @@ export interface ProfileCardProps {
   pictureUrl: string;
 }
 
+// Possible text colors to choose from for accessibility (tailwinds "neutral" colors)
+const TEXT_COLORS = [
+  "#fafafa",
+  "#f5f5f5",
+  "#e5e5e5",
+  "#d4d4d4",
+  "#a3a3a3",
+  "#737373",
+  "#525252",
+  "#404040",
+  "#262626",
+  "#171717",
+  "#0a0a0a",
+];
+
+// Possible link colors to choose from for a11y (all blues (>= 200) from tailwindcss)
+const LINK_COLORS = [
+  "#bfdbfe",
+  "#93c5fd",
+  "#60a5fa",
+  "#3b82f6",
+  "#2563eb",
+  "#1d4ed8",
+  "#1e40af",
+  "#1e3a8a",
+  "#172554",
+];
+
 export const ProfileCard = ({
   backgroundColor,
   email,
@@ -14,40 +43,53 @@ export const ProfileCard = ({
   name,
   phoneNumber,
   pictureUrl,
-}: ProfileCardProps) => (
-  <article
-    style={{ backgroundColor }}
-    className="flex w-full max-w-sm flex-col items-center rounded-lg border border-gray-200 bg-white p-6 pb-10 text-center shadow"
-  >
-    <img
-      className="mb-6 size-24 min-h-24 min-w-24 rounded-full shadow-lg"
-      src={pictureUrl}
-    />
-    <h2
-      title={name}
-      className="mb-1 w-full overflow-hidden text-ellipsis text-xl font-medium text-gray-900"
+}: ProfileCardProps) => {
+  const linkColor = backgroundColor
+    ? tinyColor.mostReadable(backgroundColor, LINK_COLORS).toHexString()
+    : LINK_COLORS[6];
+
+  return (
+    <article
+      style={{
+        backgroundColor,
+        color: backgroundColor
+          ? tinyColor.mostReadable(backgroundColor, TEXT_COLORS).toHexString()
+          : TEXT_COLORS[9],
+      }}
+      className="flex w-full max-w-sm flex-col items-center rounded-lg border border-gray-200 p-6 pb-10 text-center shadow"
     >
-      {name}
-    </h2>
-    <span
-      title={location}
-      className="mb-4 w-full overflow-hidden text-ellipsis text-sm text-gray-500"
-    >
-      {location}
-    </span>
-    <a
-      className="mb-1 w-full overflow-hidden text-ellipsis text-sm font-medium text-blue-600 hover:underline"
-      title={email}
-      href={`mailto:${email}`}
-    >
-      {email}
-    </a>
-    <a
-      className="w-full overflow-hidden text-ellipsis text-sm font-medium text-blue-600 hover:underline"
-      title={email}
-      href={`tel:${phoneNumber}`}
-    >
-      {phoneNumber}
-    </a>
-  </article>
-);
+      <img
+        className="mb-6 size-24 min-h-24 min-w-24 rounded-full shadow-lg"
+        src={pictureUrl}
+      />
+      <h2
+        title={name}
+        className="mb-1 w-full overflow-hidden text-ellipsis text-xl font-medium"
+      >
+        {name}
+      </h2>
+      <span
+        title={location}
+        className="mb-4 w-full overflow-hidden text-ellipsis text-sm"
+      >
+        {location}
+      </span>
+      <a
+        style={{ color: linkColor }}
+        className="mb-1 w-full overflow-hidden text-ellipsis text-sm font-medium hover:underline"
+        title={email}
+        href={`mailto:${email}`}
+      >
+        {email}
+      </a>
+      <a
+        style={{ color: linkColor }}
+        className="w-full overflow-hidden text-ellipsis text-sm font-medium hover:underline"
+        title={email}
+        href={`tel:${phoneNumber}`}
+      >
+        {phoneNumber}
+      </a>
+    </article>
+  );
+};
